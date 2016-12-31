679f4aea78c4f67a6d4c07cc0ff39aa41dafdada
rsthakur83 <rsthakur83@yahoo.com>
2016-12-31 19:02:19 +0700

    Update myDeployment.sh

--- myDeployment.sh
+++ myDeployment.sh
@@ -1,4 +1,4 @@
-cd /tmp/app
-cd /root/ansible-project-tomcat/web
-ansible-playbook -i inventory_gol gamer.yaml
+/usr/bin/sudo cd /tmp/app
+/usr/bin/sudo cd /root/ansible-project-tomcat/web
+/usr/bin/sudo  ansible-playbook -i inventory_gol gamer.yaml
 
